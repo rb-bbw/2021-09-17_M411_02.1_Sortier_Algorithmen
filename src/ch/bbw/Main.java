a534e6bd620314729308b8ed0d8e3b830610f140
rb-bbw <remo.bergamin@bbwch.onmicrosoft.com>
2021-09-24 14:18:35 +0200

    Implement bubblesort

--- src/ch/bbw/Main.java
+++ src/ch/bbw/Main.java
@@ -3,6 +3,25 @@ package ch.bbw;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] data = {432, 2342, 1234, 234, 42, 432, 234};
+        int[] bubblesorted = bubblesort(data);
+
+        for(int i: bubblesorted){
+            System.out.print(i + " ");
+        }
+    }
+    private static int[] bubblesort(int[] data){
+        int temp;
+        for(int i = data.length; i > 0; i--){
+            for(int j = 0; j < i-1; j++){
+                if(data[j] > data[j+1]){
+                    temp = data[j];
+                    data[j] = data[j+1];
+                    data[j+1] = temp;
+                }
+            }
+        }
+
+        return data;
     }
 }
